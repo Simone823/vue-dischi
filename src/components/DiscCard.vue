544d3f98f5e6ea0cb3601efb957a2f3c2496d1a0
Simone823 <simone.daglio823@outlook.it>
2022-03-10 17:56:03 +0100

    discCard

---
+++ src/components/DiscCard.vue
@@ -0,0 +1,79 @@
+<template>
+
+    <!-- Disc -->
+    <div class="disc_card">
+        <!-- Img wrapper -->
+        <div class="img_wrapper">
+            <img :src="poster" alt=""> 
+        </div>
+
+        <!-- Info wrapper card -->
+        <div class="info_wrapper">
+            <h3 class="title">{{title}}</h3>
+            <h3 class="author">{{author}}</h3>
+            <h3 class="year">{{year}}</h3>
+        </div>
+
+    </div>
+
+</template>
+
+
+<script>
+export default {
+    name: "DiscCard",  
+    
+    props: {
+        poster: String,
+        title: String,
+        author: String,
+        year: String,
+    },
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+@import "../assets/scss/common.scss";
+
+.disc_card {
+    width: calc(100% / 5);
+    height: 370px;
+    background-color: $color-blue-turquoise;
+    padding: 15px 0;
+
+    .img_wrapper {
+        width: 80%;
+        margin: 0 auto;
+        margin-bottom: 15px;
+        box-shadow: 0 0 5px black;
+    }
+
+    .info_wrapper {
+        text-align: center;
+        color: $color-white;
+        letter-spacing: 1px;
+
+        .title {
+            text-transform: uppercase;
+            margin-bottom: 15px;
+            text-shadow: 0 0 5px black;
+        }
+
+        .author {
+            color: $color-gray;
+            font-size: 18px;
+            margin-bottom: 5px;
+            text-shadow: 0 0 5px black;
+        }
+
+        .year {
+            color: $color-gray;
+            font-size: 16px;
+            text-shadow: 0 0 5px black;
+        }
+    }
+}
+
+</style>
