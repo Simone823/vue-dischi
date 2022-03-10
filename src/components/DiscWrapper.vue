41b8489b0405c32d6d03d11bd9277f30fec10ad4
Simone823 <simone.daglio823@outlook.it>
2022-03-10 18:01:39 +0100

    discWrapper, import discCard

---
+++ src/components/DiscWrapper.vue
@@ -0,0 +1,66 @@
+<template>
+
+    <!-- Wrapper disc -->
+    <div class="disc_wrapper">
+        <DiscCard v-for="(element, index) in arrayDisc" :key="index"
+            :poster="element.poster"
+            :title="element.title"
+            :author="element.author"
+            :year="element.year"
+        />
+
+    </div>
+
+</template>
+
+
+
+<script>
+import DiscCard from "./DiscCard.vue";
+
+// Axios
+import axios from 'axios';
+
+export default {
+    name: "DiscWrapper",
+    components: {
+        DiscCard,
+    },
+
+    data() {
+        return {
+            arrayDisc: [],
+        }
+    },
+
+    methods: {
+        chimataDiscCard: function() {
+
+        },
+    },
+
+    created() {
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then(res => {
+            console.log(res.data.response);
+            this.arrayDisc = res.data.response;
+        })
+    },
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+
+.disc_wrapper {
+    // border: 2px solid orange;
+    max-width: 1168px;
+    flex-grow: 1;
+    display: flex;
+    flex-wrap: wrap;
+    row-gap: 20px;
+    gap: 30px;
+}
+
+</style>
