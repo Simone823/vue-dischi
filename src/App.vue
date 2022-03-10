0b6e4f11f3fd6f840288d14c07e8ea940c7a1e23
Simone823 <simone.daglio823@outlook.it>
2022-03-10 16:23:00 +0100

    header main, import LogoHedaer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <template>
+
+  <!-- App Vue -->
   <div id="app">
+    <!-- Header -->
+    <header>
+      <LogoHeader/>
+    </header>
+
+    <!-- Main parte centrale -->
+    <main>
+
+    </main>
     
   </div>
+
 </template>
 
+
+
 <script>
+import LogoHeader from "./components/Logo header.vue";
 
 export default {
   name: 'App',
   components: {
-    
+    LogoHeader,
   }
 }
 </script>
+
+
 
 <style lang="scss">
 // Scss impostazioni generali
@@ -29,5 +46,12 @@ export default {
   font-family: "Antic Slab", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
+
+  header {
+    padding: 15px 20px;
+    background-color: $color-blue-turquoise;
+  }
 }
 </style>
