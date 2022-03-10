facf14682f4eb9e904f628a6a593814108f98afb
Simone823 <simone.daglio823@outlook.it>
2022-03-10 16:23:47 +0100

    logoheader component

---
+++ src/components/Logo header.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <!-- Logo Header -->
+    <div class="logo">
+        <img src="../assets/img/spotify.svg" alt="">
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "LogoHeader",
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+.logo {
+    width: 48px;
+    height: 48px;
+}
+
+</style>
