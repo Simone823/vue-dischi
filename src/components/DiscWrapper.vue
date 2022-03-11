541cafc6ed716e9a10baa61ab0f4ac65bbd7f057
Simone823 <simone.daglio823@outlook.it>
2022-03-12 00:09:27 +0100

    funzione recuperoValue e computed filterValue

--- src/components/DiscWrapper.vue
+++ src/components/DiscWrapper.vue
@@ -1,8 +1,13 @@
 <template>
 
+
     <!-- Wrapper disc -->
     <div class="disc_wrapper">
-        <DiscCard v-for="(element, index) in arrayDisc" :key="index"
+    
+        <!-- SelectOption -->
+        <SelectOption @filtra="recuperoValue"/>
+
+        <DiscCard v-for="(element, index) in filterValue" :key="index"
             :poster="element.poster"
             :title="element.title"
             :author="element.author"
@@ -20,26 +25,57 @@ import DiscCard from "./DiscCard.vue";
 
 // Axios
 import axios from 'axios';
+import SelectOption from './SelectOption.vue';
 
 export default {
     name: "DiscWrapper",
     components: {
         DiscCard,
+        SelectOption,
     },
 
     data() {
         return {
+
+            // Array oggetti discCard
             arrayDisc: [],
+            
+            // Recupero valueSelect
+            valueRecuperato: "",
         }
     },
 
     methods: {
+
+        // Chiamata al server api
         chimataDiscCard: function() {
             axios.get("https://flynn.boolean.careers/exercises/api/array/music")
             .then(res => {
                 console.log(res.data.response);
                 this.arrayDisc = res.data.response;
             })
+        },
+
+        // Recupero valueSelect
+        recuperoValue: function (valueSelect) {
+            this.valueRecuperato = valueSelect;
+            console.log(this.valueRecuperato);
+        },
+    },
+
+    computed: {
+        // Filtra valueOption
+        filterValue: function() {
+            if (this.valueRecuperato === "") {
+                return this.arrayDisc;
+            } 
+
+            return this.arrayDisc.filter((element) => {
+                const { genre } = element;
+                return genre.toLowerCase().includes(this.valueRecuperato.toLowerCase());
+
+            })
+
         },
     },
 
